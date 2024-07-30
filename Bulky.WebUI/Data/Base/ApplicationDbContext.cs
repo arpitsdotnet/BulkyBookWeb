5dd4d2c187f8aa4e171d71146e833864a5a2d6ff
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-30 12:52:19 +0530

    ApplicationDBContext Setup EFCore.

---
+++ Bulky.WebUI/Data/Base/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bulky.WebUI.Models.Masters;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.WebUI.Data.Base;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
