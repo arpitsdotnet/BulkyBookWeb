96ba87da26546f6c3567a4609f8b45867c2de767
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-30 13:12:37 +0530

    Display All Categories

--- Bulky.WebUI/Controllers/Masters/CategoryController.cs
+++ Bulky.WebUI/Controllers/Masters/CategoryController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.WebUI.Data.Base;
+using Bulky.WebUI.Models.Masters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.WebUI.Controllers.Masters;
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _dbContext;
+
+    public CategoryController(
+        ApplicationDbContext dbContext
+        )
+    {
+        _dbContext = dbContext;
+    }
     public IActionResult Index()
     {
-        return View();
+        List<Category> categories = _dbContext.Categories.ToList();
+
+        return View(categories);
     }
 }
