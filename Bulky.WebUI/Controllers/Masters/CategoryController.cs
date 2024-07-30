40b33c64f71e27c7a8aec38b448cf2f9061d8f63
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-30 12:53:18 +0530

    Category Table Added via Migration and Empty View Added to Views Folder.

---
+++ Bulky.WebUI/Controllers/Masters/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.WebUI.Controllers.Masters;
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
