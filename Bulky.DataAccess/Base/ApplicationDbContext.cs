9a691bf532e76c547f154b341e58e75b19de451b
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-08-05 15:05:48 +0530

    Identity User Extend Class Added.

--- Bulky.DataAccess/Base/ApplicationDbContext.cs
+++ Bulky.DataAccess/Base/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using BulkyBook.Models.Masters;
+﻿using BulkyBook.Models.Identity;
+using BulkyBook.Models.Masters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
