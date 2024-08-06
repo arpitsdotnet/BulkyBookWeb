e7b77491ff57bad76d00d8105b5f1fecbaa307ab
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-08-06 16:23:56 +0530

    Add ShoppingCart Model and Table.

--- Bulky.DataAccess/Base/ApplicationDbContext.cs
+++ Bulky.DataAccess/Base/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Company> Companies { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Bulky.Models/Masters/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyBook.Models.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BulkyBook.Models.Masters;
+public class ShoppingCart
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+    [ForeignKey(nameof(ProductId))]
+    [ValidateNever]
+    public Product Product { get; set; }
+
+    [Range(1,1000,ErrorMessage = "Please enter a value between 1 and 1000")]
+    public int Count { get; set; }
+
+    public string ApplicationUserId { get; set; }
+    [ForeignKey(nameof(ApplicationId))]
+    [ValidateNever]
+    public ApplicationUser ApplicationUser { get; set; }
+
+}
