b726caa5f0e62bbe533f11c4c1ea953f6fe21851
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-08-06 17:56:24 +0530

    Remove the EF ChangeTracker

--- Bulky.DataAccess/Abstracts/IRepository.cs
+++ Bulky.DataAccess/Abstracts/IRepository.cs
@@ -4,7 +4,7 @@ namespace BulkyBook.DataAccess.Abstracts;
 public interface IRepository<T> where T:class
 {
     IEnumerable<T> GetAll(string? includeProperties = null);
-    T Get(Expression<Func<T,bool>> filter, string? includeProperties = null);
+    T Get(Expression<Func<T,bool>> filter, string? includeProperties = null, bool tracked = false);
 
     void Add(T item);
     //void Update(T item); SaveChanges 
