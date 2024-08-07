88cac5d10cc2b7ce8f9f85d18a997f251df6206d
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-08-07 14:22:58 +0530

    OrderStatusWorkflow image and OrderStatus and PaymentStatus Added to StaticData file.

--- Bulky.Utilities/SD.cs
+++ Bulky.Utilities/SD.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BulkyBook.Utilities;
+﻿namespace BulkyBook.Utilities;
 public class SD
 {
     public class Role
@@ -18,5 +12,24 @@ public class SD
     {
         public const string Date = "dd-MMM-yyyy";
         public const string Time = "hh:mm tt";
+    }
+
+    public class OrderStatus
+    {
+        public const string Pending = "Pending";
+        public const string Approved = "Approved";
+        public const string InProcess = "Processing";
+        public const string Shipped = "Shipped";
+        public const string Cancelled = "Cancelled";
+        public const string Refunded = "Refunded";
+    }
+
+    public class PaymentStatus
+    {
+        public const string Pending = "Pending";
+        public const string Approved = "Approved";
+        public const string DelayedPayment = "ApprovedForDelayedPayment";
+        public const string Rejected = "Rejected";
+
     }
 }
